f00c2378e80755d02e159f07ee45335ff02d4510
vioSpark <40240957+vioSpark@users.noreply.github.com>
2019-04-30 13:56:06 +0200

    csv export added

--- source_code.py
+++ source_code.py
@@ -29,8 +29,11 @@ for row in el.df.iterrows():
             row[1][:-3] * esl.light) * tau) / 10000  # the /10000 because of the percentages
         fi_lambda = fi_lambda.append(spectral_values.append(row[1][-3:]), ignore_index=True)
     except KeyError as e:
-        print(e)
-print('fi_lambda done')
+        # !!!!!!!!!! change the commented line, to ignore inconsistencies !!!!!!!!!!
+        # print(e)
+        raise e
+fi_lambda.to_csv()
+print('fi_lambda saved')
 
 X_Y_Z_data = pd.DataFrame(columns=['X', 'Y', 'Z', 'x', 'y', 'z', 'Name', 'Gloss', 'measurement number'])
 for row in fi_lambda.iterrows():
@@ -38,7 +41,8 @@ for row in fi_lambda.iterrows():
     append_me = pc.calculate_xyz(esl, row[1][:-3])
     append_me = append_me + list(row[1][-3:].values)
     X_Y_Z_data.loc[len(X_Y_Z_data)] = append_me
-print('X, Y, Z done')
+X_Y_Z_data.to_csv()
+print('X, Y, Z data saved')
 
 L_a_b_data = pd.DataFrame(columns=['L', 'a', 'b', 'Name', 'Gloss', 'measurement number'])
 Xn, Yn, Zn = pc.calculate_xyz(esl, esl.light)[:3]
@@ -49,7 +53,8 @@ for row in X_Y_Z_data.iterrows():
     b = 200 * ((Y / Yn) ** (1 / 3) - (Z / Zn) ** (1 / 3))
     append_me = [L, a, b] + list(row[1][-3:].values)
     L_a_b_data.loc[len(L_a_b_data)] = append_me
-print('L, a, b done')
+L_a_b_data.to_csv()
+print('L, a, b data saved')
 
 names = set(L_a_b_data['Name'])
 delta = []
@@ -68,4 +73,5 @@ for name in names:
     diffs.extend([name, 'SCI'])
     delta.append(diffs)
 final_final_delta_master_values_v3 = pd.DataFrame(delta)
-raise NotImplementedError('remove the exception handling from fi_lambda!!!!4!4!4!4')
+final_final_delta_master_values_v3.to_csv()
+print('final delta values saved')
