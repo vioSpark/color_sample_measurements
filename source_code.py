fa6c4e39cf2ab07de1d0d6d34bf9a64317484491
vioSpark <40240957+vioSpark@users.noreply.github.com>
2019-05-10 01:44:53 +0200

    Visualisation based on Ági's recommendation is done

--- source_code.py
+++ source_code.py
@@ -8,24 +8,33 @@ import scipy as sc
 from excel_loader import ExcelLoader
 from txt_loader import TextFileLoader
 from external_sources_loader import ExternalSourcesLoader
+from visualiser import Transformer
 import math_pieces as pc
 
 el = ExcelLoader(
     ['data/final_export_partial/excel_folder/SZIE_2019_02_27.xls',
-     'data/final_export_partial/excel_folder/SZIE_2019_03_13.xls'])
+     'data/final_export_partial/excel_folder/SZIE_2019_03_13.xls',
+     'data/final_export_partial/excel_folder/SZIE_2019_04_24.xls'])
 el.interpolate()
-tl = TextFileLoader(['data/final_export_partial/02.27/', 'data/final_export_partial/03.13/'])
+tl = TextFileLoader(
+    ['data/final_export_partial/02.27/', 'data/final_export_partial/03.13/', 'data/final_export_partial/04.24/'])
 esl = ExternalSourcesLoader()
-fi_lambda = pd.DataFrame(columns=[*np.linspace(360, 740, 77), 'Name', 'Gloss',
+fi_lambda = pd.DataFrame(columns=[*np.linspace(360, 740, 77), 'Name', 'RT', 'Gloss',
                                   'measurement number'])  # results are stored in this
+# //R/T is added
 for row in el.df.iterrows():
     # fi_lambda.append(row * tl.df.loc[row['Name']] * ll.df, sort=False)
     try:
         tau = pd.to_numeric(tl.df.loc[tl.df['measurement number'] == row[1][-1]].loc[row[1][-3]][:-1]).abs().values
 
-        spectral_values = (pd.to_numeric(
-            row[1][:-3] * esl.light) * tau) / 10000  # the /10000 because of the percentages
-        fi_lambda = fi_lambda.append(spectral_values.append(row[1][-3:]), ignore_index=True)
+        spectral_values_1 = (pd.to_numeric(
+            row[1][:-3] * esl.light)) / 100  # the /100 because of the percentages
+        spectral_values_2 = (esl.light * tau) / 100  # the /100 because of the percentages
+        fi_lambda = fi_lambda.append(spectral_values_1.append(row[1][-3:]), ignore_index=True)
+        fi_lambda.iloc[-1, -3] = 'Reflection'
+        fi_lambda = fi_lambda.append(spectral_values_2.append(row[1][-3:]), ignore_index=True)
+        fi_lambda.iloc[-1, -3] = 'Transmission'
+
     except KeyError as e:
         # !!!!!!!!!! change the commented line, to ignore inconsistencies !!!!!!!!!!
         # print(e)
@@ -34,20 +43,36 @@ for row in el.df.iterrows():
                 tau = pd.to_numeric(
                     tl.df.loc[tl.df['measurement number'] == row[1][-1]].loc[row[1][-3].replace('ZS', 'SZ')][:-1]) \
                     .abs().values
+                spectral_values_1 = (pd.to_numeric(
+                    row[1][:-3] * esl.light)) / 100  # the /100 because of the percentages
+                spectral_values_2 = (esl.light * tau) / 100  # the /100 because of the percentages
+                fi_lambda = fi_lambda.append(spectral_values_1.append(row[1][-3:]), ignore_index=True)
+                fi_lambda.iloc[-1, -3] = 'Reflection'
+                fi_lambda = fi_lambda.append(spectral_values_2.append(row[1][-3:]), ignore_index=True)
+                fi_lambda.iloc[-1, -3] = 'Transmission'
 
-                spectral_values = (pd.to_numeric(
-                    row[1][:-3] * esl.light) * tau) / 10000  # the /10000 because of the percentages
-                fi_lambda = fi_lambda.append(spectral_values.append(row[1][-3:]), ignore_index=True)
             if e.args[0].find('SZ') >= 0:
                 tau = pd.to_numeric(
                     tl.df.loc[tl.df['measurement number'] == row[1][-1]].loc[row[1][-3].replace('SZ', 'ZS')][:-1]) \
                     .abs().values
 
-                spectral_values = (pd.to_numeric(
-                    row[1][:-3] * esl.light) * tau) / 10000  # the /10000 because of the percentages
-                fi_lambda = fi_lambda.append(spectral_values.append(row[1][-3].replace('SZ', 'ZS')).append(row[1][-2:]), ignore_index=True)
+                # spectral_values = (pd.to_numeric(
+                #     row[1][:-3] * esl.light) * tau) / 10000  # the /10000 because of the percentages
+                # fi_lambda = fi_lambda.append(spectral_values.append(row[1][-3].replace('SZ', 'ZS')).append(row[1][-2:]),
+                #                              ignore_index=True)
+
+                spectral_values_1 = (pd.to_numeric(
+                    row[1][:-3] * esl.light)) / 100  # the /100 because of the percentages
+                spectral_values_2 = (esl.light * tau) / 100  # the /100 because of the percentages
+                fi_lambda = fi_lambda.append(spectral_values_1.append(row[1][-3:].replace('SZ', 'ZS')),
+                                             ignore_index=True)
+                fi_lambda.iloc[-1, -3] = 'Reflection'
+                fi_lambda = fi_lambda.append(spectral_values_2.append(row[1][-3:].replace('SZ', 'ZS')),
+                                             ignore_index=True)
+                fi_lambda.iloc[-1, -3] = 'Transmission'
+
         except KeyError as ee:
-            print("you've really fucked it up this time, aren't you?")
+            print("you've ****** it up this time, a missing measurement:")
             print(ee)
         if e.args[0][0] != '5' or e.args[0][0] != 'H':
             pass
@@ -56,43 +81,63 @@ for row in el.df.iterrows():
 fi_lambda.to_csv('data/results/fi_lambda.csv')
 print('fi_lambda saved')
 
-X_Y_Z_data = pd.DataFrame(columns=['X', 'Y', 'Z', 'x', 'y', 'z', 'Name', 'Gloss', 'measurement number'])
+X_Y_Z_data = pd.DataFrame(columns=['X', 'Y', 'Z', 'x', 'y', 'z', 'Name', 'RT', 'Gloss', 'measurement number'])
 for row in fi_lambda.iterrows():
     # numeric integrate: dx is always 5nm.. y=fi_lambda*x_overline
-    append_me = pc.calculate_xyz(esl, row[1][:-3])
-    append_me = append_me + list(row[1][-3:].values)
+    append_me = pc.calculate_xyz(esl, row[1][:-4])
+    append_me = append_me + list(row[1][-4:].values)
     X_Y_Z_data.loc[len(X_Y_Z_data)] = append_me
 X_Y_Z_data.to_csv('data/results/X_Y_Z_data.csv')
 print('X, Y, Z data saved')
 
-L_a_b_data = pd.DataFrame(columns=['L', 'a', 'b', 'Name', 'Gloss', 'measurement number'])
+faulty = 0  # it stores, how many records haven't passed the lightness test
+L_a_b_data = pd.DataFrame(columns=['L', 'a', 'b', 'Name', 'RT', 'Gloss', 'measurement number'])
 Xn, Yn, Zn = pc.calculate_xyz(esl, esl.light)[:3]
 for row in X_Y_Z_data.iterrows():
     X, Y, Z = row[1][:3]
+    if Y / Yn < 0.01 or X / Xn < 0.01 or Z / Zn < 0.01:
+        faulty += 1
     L = 116 * (Y / Yn) ** (1 / 3) - 16
     a = 500 * ((X / Xn) ** (1 / 3) - (Y / Yn) ** (1 / 3))
     b = 200 * ((Y / Yn) ** (1 / 3) - (Z / Zn) ** (1 / 3))
-    append_me = [L, a, b] + list(row[1][-3:].values)
+    append_me = [L, a, b] + list(row[1][-4:].values)
     L_a_b_data.loc[len(L_a_b_data)] = append_me
+print('faulty ones: ' + str(faulty) + ' of ' + str(L_a_b_data.shape[0]))
 L_a_b_data.to_csv('data/results/L_a_b_data.csv')
 print('L, a, b data saved')
 
 names = set(L_a_b_data['Name'])
 delta = []
 for name in names:
-    # hardcoded SCE
-    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCE')
+    # hardcoded SCE Transmission
+    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCE') & (L_a_b_data.RT == 'Transmission')
     delta_Lab = (L_a_b_data[mask].sort_values('measurement number')).iloc[:, 0:3].diff()[1:]
     diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
-    diffs.extend([name, 'SCE'])
+    diffs.extend([name, 'SCE', 'Transmission'])
     delta.append(diffs)
 
-    # hardcoded SCI
-    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCI')
+    # hardcoded SCE Reflection
+    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCE') & (L_a_b_data.RT == 'Reflection')
     delta_Lab = (L_a_b_data[mask].sort_values('measurement number')).iloc[:, 0:3].diff()[1:]
     diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
-    diffs.extend([name, 'SCI'])
+    diffs.extend([name, 'SCE', 'Reflection'])
     delta.append(diffs)
+
+    # hardcoded SCI Transmission
+    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCI') & (L_a_b_data.RT == 'Transmission')
+    delta_Lab = (L_a_b_data[mask].sort_values('measurement number')).iloc[:, 0:3].diff()[1:]
+    diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
+    diffs.extend([name, 'SCI', 'Transmission'])
+    delta.append(diffs)
+
+    # hardcoded SCI Reflection
+    mask = (L_a_b_data.Name == name) & (L_a_b_data.Gloss == 'SCI') & (L_a_b_data.RT == 'Reflection')
+    delta_Lab = (L_a_b_data[mask].sort_values('measurement number')).iloc[:, 0:3].diff()[1:]
+    diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
+    diffs.extend([name, 'SCI', 'Reflection'])
+    delta.append(diffs)
+
 final_final_delta_master_values_v3 = pd.DataFrame(delta)
 final_final_delta_master_values_v3.to_csv('data/results/final_delta.csv')
 print('final delta values saved')
+tr = Transformer(L_a_b_data)
---
+++ visualiser.py
@@ -0,0 +1,76 @@
+"""
+Konzi Ágival:
+TDK leshet belőle
+L_a_b koordinátákra egy átlag, és +- szórás
+0.01-es errort megnézni
+
+színminták közti delta E-k et számolni, és vizualizálni (pl 1-estől mennyire különbözik a 2-es, attól mennyire a 3-as ,stb.....)
+Ezt megnézni Márk 1. mintájára --> alapból jó volt-e a mérés
+Ennek az átlagát, és szórását nézni, és így lehet összehasonlítani a különböző színeket
+
+Prezibe ezt, meg ennek a változását lehet rakni, h result is legyen
+optional: Chroma (a) + hue (b) -> mennyire változott (párolgás miatt a L-t nem nézni)
+
+ppt-t átdobhatjuk megnézésre (pár napos delay)
+
+"""
+
+import numpy as np
+import logging
+import datetime as dt
+import sys
+import pandas as pd
+import scipy as sc
+
+
+class Transformer:
+    def __init__(self, L_a_b_data):
+        columns = list(L_a_b_data.columns.values) + ['Temperature', 'Water type', 'Color', 'Shade ID']
+        tmp = pd.concat([L_a_b_data, L_a_b_data.Name.str[:1], L_a_b_data.Name.str[1:2], L_a_b_data.Name.str[2:3],
+                         pd.DataFrame(np.nan, index=range(len(L_a_b_data)), columns=['A'])], axis=1)
+        tmp.columns = columns
+        # get the Shade ID-s fixed ... I have no ide why have I calculated this....
+        tmp.update(pd.to_numeric(tmp[tmp['Color'] == 'P'].loc[:, 'Name'].str[3:].rename('Shade ID')))
+        tmp.update(pd.to_numeric(tmp[tmp['Color'] == 'Z'].loc[:, 'Name'].str[4:].rename('Shade ID')))
+        tmp.sort_values(by=['RT', 'Gloss', 'Color', 'Water type', 'Temperature', 'measurement number', 'Shade ID'],
+                        inplace=True)
+
+        final = pd.DataFrame(
+            columns=['1->2', '2->3', '3->4', '4->5', '5->6', '6->7', '7->8', '8->9', '9->10', '10->11', 'RT',
+                     'Gloss', 'Color', 'Water type', 'Temperature', 'measurement number'])
+        measurements = tmp.loc[:, 'measurement number'].max()
+        gloss_type = ('SCI', 'SCE')
+        rt_type = ('Transmission', 'Reflection')
+        temp_type = ('5', 'H')
+        water_type = ('+', 'D', 'M')
+        color_type = ('P', 'Z')
+        index = 0
+        for gloss in range(2):
+            for rt in range(2):
+                for temp in range(2):
+                    for water in range(3):
+                        for color in range(2):
+                            for meas_num in range(measurements):
+                                mask = (tmp.Gloss == gloss_type[gloss]) & \
+                                       (tmp.RT == rt_type[rt]) & \
+                                       (tmp.loc[:, 'Temperature'] == temp_type[temp]) & \
+                                       (tmp.loc[:, 'Water type'] == water_type[water]) & \
+                                       (tmp.loc[:, 'Color'] == color_type[color]) & \
+                                       (tmp.loc[:, 'measurement number'] == meas_num)
+                                res = tmp.loc[mask]
+                                # name = res.loc['Name']
+                                delta_Lab = res.loc[:, 'L':'b']
+                                diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
+                                final.loc[index] = diffs + [rt_type[rt], gloss_type[gloss], color_type[color],
+                                                            water_type[water], temp_type[temp], meas_num]
+                                index += 1
+        """
+        method for one:
+        mask = (tmp.Gloss == 'SCI') & (tmp.RT == 'Reflection') & (tmp['measurement number'] == 0) & (
+                tmp['Temperature'] == '5') & (tmp['Water type'] == 'D') & (tmp['Color'] == 'P')
+        res = tmp.loc[mask]
+        delta_Lab = res.loc[:, 'L':'b']
+        diffs = list(delta_Lab.applymap(lambda x: x ** 2).sum(1).apply(np.sqrt))
+        """
+        final.to_csv('data/results/final_final.csv')
+        asd = 6
